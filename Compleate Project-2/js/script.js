d3fd7885c9c3a3024639600bec899e13052eb7a9
Arif3636 <75058626+arifrahman1989@users.noreply.github.com>
2025-04-02 11:24:04 +0600

    Add files via upload
    This is HTML Template Projects

---
+++ Compleate Project-2/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    
+    var menu_position = $('#menu').offset().top;
+    
+    $(window).on('.scroll',function(){
+        
+        var winscroll = $(window).scrollTop();
+        
+        if(winscroll > menu_position){
+            
+            $('#menu').addclass('.fix');
+        }
+        
+    });
+        
+    function 
+    
+    
+    
+    
+    
+});
